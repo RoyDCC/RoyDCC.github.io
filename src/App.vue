dfeae66eef3e6f0934fab1b3f21fa00719549a93
Rodrigo Diaz de Leon C <rodrigo.diazdeleonc@gmail.com>
2025-08-04 23:09:02 -0600

    Solve import error in app.js

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script setup lang="ts">
-import HomePage from "./ui/home/pages/HomePage.vue";
 import Navbar from "./shared/components/Navbar.vue";
 import Footer from "./shared/components/Footer.vue";
 </script>
