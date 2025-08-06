5cbba15d54a277122ed2b58b131d9cc64642ca14
Rodrigo Diaz de Leon C <rodrigo.diazdeleonc@gmail.com>
2025-08-06 00:30:53 -0600

    solve problem with props declaration in resourcecard component

--- src/ui/resources/components/ResourceCard.vue
+++ src/ui/resources/components/ResourceCard.vue
@@ -4,21 +4,21 @@
     glass-border-full rounded-xl bg-soft-blue 
     flex flex-col flex-grow relative gap-3 justify-between"
   >
-    <a :href="resource.pageLink" target="_blank" class="external-link w-6 h-6 absolute">
+    <a :href="props.resource.pageLink" target="_blank" class="external-link w-6 h-6 absolute">
       <img :src="externalLink" alt="Visit" class="w-full" />
     </a>
 
-    <h1 class="text-white text-xl font-semibold">{{ resource.pageName }}</h1>
-    <p class="text-desc">{{ resource.description }}</p>
+    <h1 class="text-white text-xl font-semibold">{{ props.resource.pageName }}</h1>
+    <p class="text-desc">{{ props.resource.description }}</p>
     <ul class="flex flex-wrap text-sm gap-2">
       <li
-        v-for="t in resource.tags"
+        v-for="t in props.resource.tags"
         class="bg-purple text-purple border-purple font-light rounded-full p-1 px-3 capitalize"
       >
         {{ t }}
       </li>
     </ul>
-    <p class="text-desc capitalize">{{ resource.category }}</p>
+    <p class="text-desc capitalize">{{ props.resource.category }}</p>
   </div>
 </template>
 
