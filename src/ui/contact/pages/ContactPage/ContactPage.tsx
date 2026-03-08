fcdee0f0dd1477ac459a146ccb61d652cb9efb87
RoyDCC <rodrigo.diazdeleonc@gmail.com>
2026-03-08 17:10:14 -0600

    feat(contact): contact page and contact card components

--- src/ui/contact/pages/ContactPage/ContactPage.tsx
+++ src/ui/contact/pages/ContactPage/ContactPage.tsx
@@ -1,9 +1,23 @@
 import type { FC } from "react";
+import { ContactCard } from "../components/CiontactCard";
+import { contactOptions } from "@/domain/shared/constants/contact-options";
 
 export const ContactPage: FC = () => {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <section className="text-center container-lg px-4 py-10 mt-15">
+      <div className="contact-text-container">
+        <h1 className="text-5xl text-white font-bold">Contacto</h1>
+        <p className="text-desc text-xl mt-3">
+          ¿Tienes algún proyecto en mente o quieres colaborar?
+          <br />
+          ¡Me encantaría escuchar de ti!
+        </p>
+      </div>
+      <div className="contact-cards-container flex flex-row flex-wrap justify-center gap-5 py-12">
+        {contactOptions.map((opt) => (
+          <ContactCard title={opt.title} description={opt.description} link={opt.link} icon={opt.icon} />
+        ))}
+      </div>
+    </section>
   );
 };
--- src/ui/shared/components/Footer/Footer.tsx
+++ src/ui/shared/components/Footer/Footer.tsx
@@ -8,10 +8,10 @@ export const Footer: FC = () => {
     <div className="footer-wrapper max-w-7xl m-auto flex flex-col md:flex-row justify-between items-center gap-8 p-8">
       <div className="links-container flex flex-row gap-5 justify-center items-center">
         <a href="https://github.com/RoyDCC" target="_blank" className="inline-block w-10 h-10">
-          <img src="/icons/github.svg" alt="Github Link" className="opacity-65"/>
+          <img src="/icons/github.svg" alt="Github Link" className="opacity-55 invert brightness-0"/>
         </a>
         <a href="#" className="inline-block w-10 h-10">
-          <img src="/icons/linkedin.svg" alt="Linkedin Link" className="opacity-65"/>
+          <img src="/icons/linkedin.svg" alt="Linkedin Link" className="opacity-55 invert brightness-0"/>
         </a>
       </div>
 
